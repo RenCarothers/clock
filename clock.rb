973aa1538cc66b7f6ef9f681213a2ab086bc2e0f
Ren Carothers <54607085+RenCarothers@users.noreply.github.com>
2020-09-14 17:06:39 -0700

    Update clock.rb

--- clock.rb
+++ clock.rb
@@ -1,1 +1,25 @@
 # clock.rb
+# clock.rb
+
+def clock(hours, minutes, seconds)
+  time_fields = [hours, minutes, seconds]
+  max_values = [23, 59, 59]
+
+  time_fields.each_with_index do |field, index|
+    if field > max_values[index]
+      raise ArgumentError.new("#{field} is too large")
+    elsif field < 0
+      raise ArgumentError.new("#{field} is invalid (negative)")
+    end
+  end
+
+  time_fields.map! do |field|
+    if field < 10
+      "0#{field}"
+    else
+      "#{field}"
+    end
+  end
+
+  return "#{time_fields[0]}:#{time_fields[1]}:#{time_fields[2]}"
+end
